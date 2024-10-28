604eb2d0bad343d64ef76d023aa027902a1720ed
Lúcio Beckler Passos <luciobecklerp@gmail.com>
2024-10-28 12:40:37 -0300

    feat: configurando end-points, adicionado arquivo de imagem e permitindo o acesso remoto a estes arquivos.

---
+++ Quizz.API/Quizz.API/Migrations/20241022185809_Initial Create.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quizz.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Score = table.Column<int>(type: "int", nullable: false),
+                    TimeTaken = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tittle = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Option1 = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Option2 = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Option3 = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Option4 = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
---
+++ Quizz.API/Quizz.API/Models/Question.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Quizz.API.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string Tittle { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string?  ImageName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Option1 { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Option2 { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Option3 { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Option4 { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Answer { get; set; } = "";
+
+    }
+}
